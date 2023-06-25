1d4079c2146d798433662ebb4d171d874f3547a7
iamazharuddin <iamazharuddin10@gmail.com>
2023-06-25 08:08:41 +0530

    Initial Commit

---
+++ WeatherSwiftUIApp/WeatherSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIAppApp.swift
+//  WeatherSwiftUIApp
+//
+//  Created by Azharuddin 1 on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
